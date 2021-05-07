49b27826a044ea9d72808c71b1dd72e540516429
shawn-chambers <chambers.shawn.89@gmail.com>
2021-05-07 13:40:53 -0700

    applied styles to check compilation

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve('client/app.js'),
+  entry: path.resolve('src/app.js'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
