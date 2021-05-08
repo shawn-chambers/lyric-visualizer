7e8adc4e2db3c75388be4698ed74c66cf29aac7d
shawn-chambers <chambers.shawn.89@gmail.com>
2021-05-08 11:52:02 -0700

    added route to fetch lyrics by song id

--- server/routes/lyricsRoutes.js
+++ server/routes/lyricsRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { getWordsByQuery } = require('../controllers/lyricsControllers');
+const { getWordsByQuery, getLyricsFromId } = require('../controllers/lyricsControllers');
 
 router
   .route('/')
-  .get(getWordsByQuery);
+  .get(getWordsByQuery)
+router
+  .route('/:id')
+  .get(getLyricsFromId)
 
 module.exports = router;
