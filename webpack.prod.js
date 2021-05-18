b0a6f8293266094144199c9d669b6b6ffd1413e3
shawn-chambers <chambers.shawn.89@gmail.com>
2021-05-18 09:02:43 -0700

    added heroku postbuild script

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,10 @@
 const merge = require('webpack-merge');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const CompressionPlugin = require("compression-webpack-plugin");
 const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
-    new BundleAnalyzerPlugin(),
     new CompressionPlugin(),
   ]
 })
