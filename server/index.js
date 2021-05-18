eb91f1ef118ab1498974b6b090b11e3d5e8ec486
shawn-chambers <chambers.shawn.89@gmail.com>
2021-05-18 08:15:52 -0700

    server responds to req for .gz

--- server/index.js
+++ server/index.js
@@ -2,12 +2,21 @@ const express = require('express');
 require('dotenv').config()
 const port = process.env.PORT || 8080;
 const app = express();
+const compression = require('compression');
 const morgan = require('morgan');
 const lyricsRouter = require('./routes/lyricsRoutes.js');
 const songsRouter = require('./routes/songsRoutes.js');
 
+app.use(compression());
 app.use(morgan('dev'));
+app.get('*.gz', function (req, res, next) {
+  if (req.headers['x-no-compression']) next();
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/javascript');
+  next();
+});
 app.use(express.static('public'));
+
 
 app.use('/api/lyrics', lyricsRouter);
 app.use('/api/songs', songsRouter);
