620b8af05528bc77703b51594548b353f66ab66d
shawn-chambers <chambers.shawn.89@gmail.com>
2021-05-24 14:02:52 -0700

    drop down displays with css transition

--- src/components/YearSearchBar.js
+++ src/components/YearSearchBar.js
@@ -1,32 +1,60 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import { AppContext } from '../context/AppContext';
+import useDimension from '../hooks/useDimension';
+import { makeYears } from '../utils';
+
 
 const YearSearchBar = () => {
   const { fetchWordsByYear, year, setYear } = useContext(AppContext);
-  const [queryYear, setQueryYear] = useState(year);
+  const [selected, setSelected] = useState('');
 
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      setYear(queryYear);
+  const handleSelected = (e) => {
+    if (selected.length === 0) {
+      setSelected('selected');
+    } else {
+      setSelected('');
+    }
+    if (e.target.innerText !== 'Select a Year...') {
+      setYear(Number(e.target.innerText))
     }
   }
 
   useEffect(() => {
-    fetchWordsByYear(year);
+    fetchWordsByYear(year)
   }, [year])
 
+  const handleDisplay = (selected) => {
+    if (selected.length) {
+      return {
+        maxHeight: '150px',
+        height: '150px',
+        padding: '1rem',
+      }
+    } else {
+      return {
+        maxHeight: '0px',
+        height: height,
+      }
+    }
+  }
+
+  const dropDownRef = useRef(null);
+  const { height } = useDimension(dropDownRef)
+
   return (
-    <div className="search">
-      <form>
-        <input
-          value={queryYear}
-          onKeyDown={handleKeyPress}
-          onChange={(e) => setQueryYear(e.target.value)}
-          placeholder="Enter a Year..."
-        >
-        </input>
-        <button onClick={(e) => { e.preventDefault(); setYear(queryYear) }}>Search</button>
-      </form>
+    <div className={`container ${selected}`}>
+      <div className={`select`} onChange={(e) => setQueryYear(e.target.value)}
+        onClick={handleSelected}>
+        Select a Year...</div>
+      <ul style={handleDisplay(selected)} ref={dropDownRef}>
+        {makeYears(1970, 2020).map(year => {
+          return (
+            <li key={year}>
+              <div className='select__year' onClick={(e) => handleSelected(e)} value={year}>{year}</div>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
