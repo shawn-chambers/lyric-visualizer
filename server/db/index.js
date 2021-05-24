c7795b6db320c225dfbd7254be18dd86db0695ed
shawn-chambers <chambers.shawn.89@gmail.com>
2021-05-24 09:56:08 -0700

    dev db connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,16 +1,21 @@
 const { Client } = require('pg');
 require('dotenv').config();
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+
+const client = new Client()
+// Notes: Prod Heroku DB
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 
 client.connect()
   .then(() => {
-    console.log(`successful postgress connection to ${process.env.DATABASE_URL}`);
+    console.log('successful postgress connection');
+    // Notes: Prod Heroku DB
+    // console.log(`successful postgress connection to ${process.env.DATABASE_URL}`);
   })
   .catch(() => {
     console.error('unsuccessful connection');
